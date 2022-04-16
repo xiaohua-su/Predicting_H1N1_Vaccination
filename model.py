c9751b760f4be2f9eb84ca3a7e4a1d4076fee582
Xiaohua Su <xiaohuasu99@gmail.com>
2022-04-16 01:02:14 -0500

    Checked out nulls and wrote train-test split. Imported modelwithcv file

---
+++ model.py
@@ -0,0 +1,62 @@
+class ModelWithCV():
+    '''Structure to save the model and more easily see its crossvalidation'''
+
+    def __init__(self, model, model_name, X, y, cv_now=True):
+        self.model = model
+        self.name = model_name
+        self.X = X
+        self.y = y
+        # For CV results
+        self.cv_results = None
+        self.cv_mean = None
+        self.cv_median = None
+        self.cv_std = None
+        #
+        if cv_now:
+            self.cross_validate()
+
+    def cross_validate(self, X=None, y=None, kfolds=10):
+        '''
+        Perform cross-validation and return results.
+
+        Args:
+          X:
+            Optional; Training data to perform CV on. Otherwise use X from object
+          y:
+            Optional; Training data to perform CV on. Otherwise use y from object
+          kfolds:
+            Optional; Number of folds for CV (default is 10)
+        '''
+
+        cv_X = X if X else self.X
+        cv_y = y if y else self.y
+
+        self.cv_results = cross_val_score(self.model, cv_X, cv_y, cv=kfolds)
+        self.cv_mean = np.mean(self.cv_results)
+        self.cv_median = np.median(self.cv_results)
+        self.cv_std = np.std(self.cv_results)
+
+    def print_cv_summary(self):
+        cv_summary = (
+            f'''CV Results for `{self.name}` model:
+            {self.cv_mean:.5f} ± {self.cv_std:.5f} accuracy
+        ''')
+        print(cv_summary)
+
+    def plot_cv(self, ax):
+        '''
+        Plot the cross-validation values using the array of results and given
+        Axis for plotting.
+        '''
+        ax.set_title(f'CV Results for `{self.name}` Model')
+        # Thinner violinplot with higher bw
+        sns.violinplot(y=self.cv_results, ax=ax, bw=.4)
+        sns.swarmplot(
+            y=self.cv_results,
+            color='orange',
+            size=10,
+            alpha=0.8,
+            ax=ax
+        )
+
+        return ax
